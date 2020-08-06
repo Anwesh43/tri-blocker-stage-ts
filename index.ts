13ccaa7d58aa330c86dbcd81e352b53ee52e65ec
Anwesh43 <anweshthecool0@gmail.com>
2020-08-06 19:37:33 +0530

    adding TBNode class

--- index.ts
+++ index.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class TBNode {
+
+    next : TBNode 
+    prev : TBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TBNode {
+        var curr : TBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
